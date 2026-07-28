727c05e924b4c6b9ead0246c87a7e27c0f910e4d
eduardo <edavilazevallos@gmail.com>
2026-07-29 01:56:02 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => '¡Docker, Laravel y la API están funcionando perfectamente!'
+    ]);
+});
